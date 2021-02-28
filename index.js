9dcab4664bc04dfc6d469b4b90d918c1764b338c
CHRIS_LEE <chrismlee87@gmail.com>
2021-02-28 00:03:31 -0800

    page works missing youtube

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { getMovieByQuery } from './scripts.js';
+import { getMovieByQuery, getYouTubeTrailer } from './scripts.js';
 import { tmdbKey, youTubeKey } from './config.js';
 
 // Get Element References from DOM
@@ -6,19 +6,31 @@ const titleElement = document.getElementById('title');
 const yearElement = document.getElementById('year');
 const descElement = document.getElementById('desc');
 const ratingElement = document.getElementById('rating');
+const posterElement = document.getElementById('poster')
 const formElement = document.getElementById('form');
 const inputElement = document.getElementById('input-field');
+const posterLink = 'https://image.tmdb.org/t/p/w500';
 
-formElement.addEventListener('submit', (e) => {
+formElement.addEventListener('submit', async (e) => {
   e.preventDefault();
   const input = inputElement.value;
-  
+  // const data = await getMovieByQuery(tmdbKey, input);
+  // const posterPath = await getMoviePoster(poster);
 
   getMovieByQuery(tmdbKey, input)
   .then((data) => {
     titleElement.innerHTML = data.title;
     yearElement.innerHTML = data.date;
-    descElement.innerHTML = data.overview;
+    descElement.innerHTML = `<i>${data.overview}</i>`;
     ratingElement.innerHTML = `Rating: ${data.vote}`;
+    posterElement.innerHTML = `<img id='poster-load' src=${posterLink}${data.poster}></img>`;
   }).catch((err) => console.log(err.message));
-})
+});
+
+// Is there a way to grab the entire array of results and display all of the posters on the screen?
+// User inputs.
+// json returns array.
+// entire array is shown on screen (images + titles + year), equal spacing. 
+// user clicks image of which movie they wanted.
+// screen changes to single poster + youtube trailer link
+// 
--- scripts.js
+++ scripts.js
@@ -1,27 +1,12 @@
 const linkMovies = "https://api.themoviedb.org/3/search/movie?";
-const linkPoster = 'https://image.tmdb.org/t/p/w500';
 const youTubeSearch = "https://www.youtube.com/embed/";
-
+// const posterLink = 'https://image.tmdb.org/t/p/w500';
 
 async function getMovie(moviePath) {
   try {
     const res = await fetch(moviePath);
     const json = await res.json();
-    const jsonArray = json.results
-    console.log(jsonArray)
 
-    // Check the returned array for images to display
-    for (var i = 0; i < jsonArray.length; i++) {
-      const returnedArray = jsonArray[i].poster_path
-        // remove null results from images
-        if (returnedArray !== null) {
-          // return good results
-          console.log(returnedArray)
-          // return returnedArray
-        }
-    }
-
-    
     const movieData = {
       title: json.results[0].original_title,
       date: json.results[0].release_date,
@@ -29,40 +14,46 @@ async function getMovie(moviePath) {
       poster: json.results[0].poster_path,
       vote: json.results[0].vote_average,
     };
+
     return movieData
   } catch (err) {
     return err;
   }
 }
 
-
-
 function getMovieByQuery(tmdbKey, query) {
   const moviePath = `${linkMovies}api_key=${tmdbKey}&query=${query}`;
   return getMovie(moviePath);
 }
 
-// function getMoviePoster() { 
-//   const 
-//   return `${linkPoster}${poster}`;
+// function getPoster(poster)
+//   const posterPath = `${posterLink}${poster}`;
+//   console.log(posterPath);
+//   return posterPath(poster);
+
+    // This is  stretch goal now let's skip for now
+// Check the returned array for images to display
+// for (var i = 0; i < movieData.length; i++) {
+//   const returnedArray = movieData[i].poster_path
+//     // remove null results from images
+//     if (returnedArray !== null) {
+//       // return good results
+//       console.log(returnedArray)
+//       // return returnedArray
+//     }
 // }
 
-// function getYouTubeTrailer(VIDEO_ID, autoplay=1) {
-//   const playTrailer = `${youTubeSearch}${VIDEO_ID}?${autoplay}`;
-//   return playTrailer
-// }
+function getYouTubeTrailer(VIDEO_ID, autoplay=1) {
+  const playTrailer = `${youTubeSearch}${VIDEO_ID}?${autoplay}`;
+  // console.log(playTrailer, '--!!!!!!-------')
+  return playTrailer
+}
 
 
 
-
-
-
-
-// function searchYouTube() {
-
-// }
 
 export {
   getMovieByQuery,
-  getMovie,
+  // getPoster,
+  getYouTubeTrailer,
 };
